8f2013100a289f9a4e24888f0f6c0f539f9ba319
zet79 <patricia.natividad.c@gmail.com>
2025-05-14 21:02:13 -0500

    Changes in main.py

--- app/main.py
+++ app/main.py
@@ -1,33 +1,40 @@
 from fastapi import FastAPI
 from fastapi.security import HTTPBearer
-from app.core.db import init_db
-from app.routers import auth, comunidades, administradores
-from app.models.usuario import Usuario
-from fastapi import FastAPI, Depends
-from fastapi.security import OAuth2PasswordBearer, HTTPBearer
-from fastapi.openapi.models import APIKey, APIKeyIn, SecuritySchemeType
 from fastapi.openapi.utils import get_openapi
 
-app = FastAPI()
+from app.core.db import init_db
+from app.modules.auth.routers import router as auth_router
+from app.modules.communities.routers import router as comunidades_router
+from app.modules.users.routers import router as usuarios_router
+
+app = FastAPI(
+    title="CommuConnect API",
+    version="1.0.0",
+    description="API de CommuConnect"
+)
+
 security = HTTPBearer()
 
 @app.on_event("startup")
 def on_startup():
     init_db()
 
-app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
-app.include_router(comunidades.router, prefix="/api/comunidades", tags=["Comunidades"])
-app.include_router(administradores.router, prefix="/api/administradores", tags=["Administradores"])
+app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
+app.include_router(comunidades_router, prefix="/api/comunidades", tags=["Comunidades"])
+app.include_router(usuarios_router,  prefix="/api/usuarios", tags=["Usuarios"])
+
 
 def custom_openapi():
     if app.openapi_schema:
         return app.openapi_schema
+
     openapi_schema = get_openapi(
-        title="CommuConnect API",
-        version="1.0.0",
-        description="API de CommuConnect",
+        title=app.title,
+        version=app.version,
+        description=app.description,
         routes=app.routes,
     )
+
     openapi_schema["components"]["securitySchemes"] = {
         "BearerAuth": {
             "type": "http",
