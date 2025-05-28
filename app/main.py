2f666b13c0b0afe786f258c59d024031cef9a279
Ubuntu <ubuntu@ip-172-31-19-187.ec2.internal>
2025-05-28 17:20:51 +0000

    Correcciones en main.pyw

--- app/main.py
+++ app/main.py
@@ -8,22 +8,20 @@ from app.modules.users.routers import router as usuarios_router
 
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(
-    title="CommuConnect API",
-    version="1.0.0",
-    description="API de CommuConnect"
-)
 
-# Permitir solicitudes desde tu frontend
+app = FastAPI()
+
+origins = [
+    "http://commuconnect-frontend-v1.s3-website-us-east-1.amazonaws.com"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  # o ["*"] para todos los orígenes
+    allow_origins=origins,  # O usa ["*"] temporalmente para probar
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
 
 
 security = HTTPBearer()
